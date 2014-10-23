25cf61c0b5345c9830c6eed58d5d3444c2935407
Faizaan Mahmud <Fez@Faizaans-MacBook-Pro.local>
2014-10-23 00:03:36 -0500

    moved and modified scraper.py

---
+++ src/scraper.py
@@ -0,0 +1,15 @@
+import urllib2
+
+developer_key = 'E2469B841908CBF830EC652B7BAB5D71'
+for i in range(   10000000) :
+	match_id = 9200000000
+	url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key={{KEY}}&match_id={{MID}}'
+	url = url.replace("{{KEY}}", developer_key)
+	url = url.replace("{{MID}}", str(match_id+i))
+	response = urllib2.urlopen(url)
+	html = response.read()
+	f = open('game_files/'+str(match_id+i)+'.json', 'w')
+	f.write(html)
+	print "Opened match number "+ str(match_id+i)
+
+
